58447aa63022c8e491e4e5baa0dc4b2eb3bd6274
niekurzakdaniel <niekurzakdaniel@gmail.com>
2021-06-10 23:40:34 +0200

    add player ships adding

--- script.js
+++ script.js
@@ -4,36 +4,114 @@ const SELECTED_PLAYER_CELL='selectedPlayerCell'
 const DESTROYED_PLAYER_CELL='destroyedPlayerCell'
 const SHOOTED_FIELD_CELL='shootedFieldCell'
 const FIELD='field'
-const RANDOM_MAP_FILL="randomMapFill"
-const MAP_DIMENSIONS=7
+const MAP_DIMENSIONS=10
 
 const aiBoard=document.getElementById(AI_BOARD)
 const playerBoard=document.getElementById(PLAYER_BOARD)
 
+const ships=5;
+
 let aiArr=[]
 let playerArr=[]
 
-const mapGenerator=(map,random)=>{
+let userAddedShips=0
+let vertical=false;
+
+const checkMapBeforeAddShip=(playerArr,isVertical,x, y)=>{
+
+    if(isVertical){
+        for(let index=0;index<ships-userAddedShips;++index){
+            if((playerArr[x+index][y]!=0) || (x+index>=MAP_DIMENSIONS)){
+                return false
+            }
+        }
+    }
+
+    if(!isVertical){
+        for(let index=0;index<ships-userAddedShips;++index){
+            if((playerArr[x][y+index]!=0) ||(y+index>=MAP_DIMENSIONS)){
+                return false
+            }
+        }
+    }
+    
+    return true;
+}
+
+const addPlayerShips=(e)=>{
+
+
+        let i= Number(e.target.getAttribute('i'))
+        let j= Number(e.target.getAttribute('j'))
+
+        const addShip=checkMapBeforeAddShip(playerArr,!vertical,i,j);
+        if(addShip){
+            vertical=!vertical;
+            userAddedShips++;
+            e.target.className+= ' ' + SELECTED_PLAYER_CELL
+            let indexHelper=0;
+            
+            for(let index=userAddedShips;index<=ships;++index){
+                if(vertical){
+                    playerArr[i+indexHelper][j]=1
+                     const fieldItem=document.querySelector(`div[i="${i+indexHelper}"][j="${j}"][parentboard=${PLAYER_BOARD}]`)
+                     fieldItem.classList.add(SELECTED_PLAYER_CELL)
+                }else{
+                    playerArr[i][j+indexHelper]=1;
+                    const fieldItem=document.querySelector(`div[i="${i}"][j="${j+indexHelper}"][parentboard=${PLAYER_BOARD}]`)
+                    fieldItem.classList.add(SELECTED_PLAYER_CELL)
+                }
+                indexHelper++;
+            }
+
+            if(userAddedShips===ships){
+                const fieldItems=document.querySelectorAll(`div[parentboard=${PLAYER_BOARD}]`)
+                fieldItems.forEach(e=>console.log(e.removeEventListener('click',addPlayerShips)))
+            }
+        } 
+}
+
+const shoot=e=>{
+    const i= Number(e.target.getAttribute('i'))
+    const j= Number(e.target.getAttribute('j'))
+    const parentboard=e.target.getAttribute('parentboard')
+    
+    if(e.target.classList.contains(SELECTED_PLAYER_CELL)){
+        if(parentboard==PLAYER_BOARD){  
+            if(playerArr[i][j]===1){
+                playerArr[i][j]==-1;
+               
+                e.target.classList.remove(SELECTED_PLAYER_CELL);
+                e.target.classList+= ' ' + DESTROYED_PLAYER_CELL;
+            }
+        }else{
+            e.target.classList.remove(SELECTED_PLAYER_CELL);
+                e.target.classList+= ' ' + SHOOTED_FIELD_CELL;
+        }
+        
+        if(e.target.getAttribute("parentboard")===PLAYER_BOARD){
+            playerArr[i][j]=0
+        }else{
+            aiArr[i][j]=0
+        }
+
+    }
+}
+
+const mapGenerator=(map)=>{
     const mapHeight=map.clientHeight/MAP_DIMENSIONS + 'px'
     for(let i=0;i<MAP_DIMENSIONS;++i){
         for(let j=0;j<MAP_DIMENSIONS;j++){  
             const div=document.createElement(`div`)
             div.className=FIELD
-
-            if(random){
-                if(Math.floor(Math.random()*5)===0){
-                    div.classList.add(SELECTED_PLAYER_CELL)
-                    fieldsArr[i][j]=true
-                }
-            }
-
             div.setAttribute("i",i)
             div.setAttribute("j",j)
             div.setAttribute("parentboard",map.id)
             div.style.flexBasis=100/MAP_DIMENSIONS +'%'
             div.style.height= mapHeight
-          
-            
+            if(div.getAttribute("parentboard")===PLAYER_BOARD){
+                div.addEventListener('click',addPlayerShips)
+            }
             map.append(div)
         }   
     } 
@@ -42,9 +120,8 @@ const mapGenerator=(map,random)=>{
 const newGame=()=>{    
         aiArr=Array.from(Array(MAP_DIMENSIONS), () => new Array(MAP_DIMENSIONS).fill(0))
         playerArr=Array.from(Array(MAP_DIMENSIONS), () => new Array(MAP_DIMENSIONS).fill(0))
-        mapGenerator(playerBoard,false)
-        mapGenerator(aiBoard,false)
+        mapGenerator(playerBoard)
+        mapGenerator(aiBoard)
 }
 
 newGame();
-
